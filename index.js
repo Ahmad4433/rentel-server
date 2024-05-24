4cf199266c123197e5cb57bd1b024cf45ba4b00f
Ahmad4433 <aliraza126005#gmail.com>
2024-05-24 19:46:29 +0500

    origin

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const salesRepoteRoutes = require("./routes/admin/reports/salesReport");
 const getClientFilteredProductList = require("./routes/client/products");
 const app = express();
 getConnection();
-app.use(cors());
+app.use(cors({ origin: true }));
 app.use(express.json({ limit: "20mb" }));
 app.use(express.urlencoded({ extended: true, limit: "20mb" }));
 app.get("/", (req, res, next) => {
